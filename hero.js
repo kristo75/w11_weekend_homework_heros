d6a33e46a8c0f9f9deb9d50170672893585c032f
Kris <kristopher_anderson@icloud.com>
2018-02-16 12:42:32 +0000

    started new project with 3 passing tests

--- hero.js
+++ hero.js
@@ -1,11 +1,21 @@
-const Hero = function(name) {
+const Hero = function(name, favFood, health) {
   this.name = name;
+  this.favFood = favFood;
+  this.health = health;
 
 }
 
 Hero.prototype.canTalk = function () {
   return 'My name is ${this.name}, have you seen Robin?'
 };
+
+Hero.prototype.isFavFood = function (food) {
+  return food.name === this.isFavFood ? true : false;
+};
+
+Hero.prototype.hasHealth = function (health) {
+  return health.name === this.hashealth ? true : false;
+}
 
 
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -2,10 +2,24 @@ const assert = require('assert');
 const Hero = require('../hero.js');
 
 describe('Hero', function () {
-hero = new Hero('Batman')
+  
 
+
+beforeEach(function () {
+  hero1 = new Hero('Batman', 'Banana', 50);
+  hero2 = new Hero('Robin', 'Apple', 20);
+})
 
   it('should have a name', function(){
-    assert.strictEqual(hero.name, "Batman");
+    assert.strictEqual(hero1.name, 'Batman');
+  })
+
+
+  it('should have favourite food', function () {
+    assert.strictEqual(hero1.favFood, 'Banana');
+  })
+
+  it('should have health', function(){
+    assert.strictEqual(hero2.health, 20)
   })
 })
